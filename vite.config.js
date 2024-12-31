36c63bf380f3871724685255fcf66b97b19f0ac3
priteshshah96 <priteshshahwork@gmail.com>
2024-12-30 22:16:02 -0500

    custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/', // Replace <repository-name> with your GitHub repository name
+  base: '/', // Use '/' for custom domains
   plugins: [react()],
 });
